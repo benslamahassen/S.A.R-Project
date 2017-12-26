b0f0e6a900505a0c4d097d957c20c963a43b586c
benslamahassen <benslamahassen5@gmail.com>
2017-12-26 16:50:01 +0100

    Adding Menu for Client

--- CodeBase/Client.java
+++ CodeBase/Client.java
@@ -1,5 +1,6 @@
 import java.rmi.registry.*;
 import java.rmi.*;
+import java.util.Scanner;
 
 public class Client {
 
@@ -13,12 +14,6 @@ public class Client {
             FabriqueInterface fab = (FabriqueInterface) reg.lookup("Fabrique");
             //Création d'institution
             InstitutionInterface institution = (InstitutionInterface) fab.createInstitution();
-            //Création de l'Employe
-            Employe employe = new Employe("salah", "ali", 01234567, 93116960);
-            //Invocation de la méthode Add
-            String resultat = institution.add(employe);
-            //Affichage
-            System.out.println("Succée de l'ajout : \n" + resultat);
 
         } catch (Exception e) {
 
@@ -26,5 +21,44 @@ public class Client {
             System.out.println(e.toString());
 
         }
+        while (1) {
+            System.out.println("Tapez:\n"
+                    + "1: pour ajouter un employé.\n"
+                    + "2: pour supprimer un employé.\n"
+                    + "3: pour mettre à jour un employé.\n"
+                    + "4: pour chercher un employé.\n"
+                    + "5: pour quitter.");
+            Scanner choixMenuScanner = new Scanner(System.in);
+            int choixMenu = choixMenuScanner.nextInt();
+            switch (choixMenu){
+                case 1:
+                    System.out.println("Tapez le nom de l'employé à ajouter.\n"+"Nom de l'employé à ajouter":);
+                    Scanner nomEmpScanner = new Scanner(System.in);
+                    String nomEmp = nomEmpScanner.next();
+                    System.out.println("Tapez le prenom de l'employé à ajouter.\n"+"Prenom de l'employé à ajouter:");
+                    Scanner prenomEmpScanner = new Scanner(System.in);
+                    String prenomEmp = prenomEmpScanner.next();
+                    System.out.println("Tapez le CIN de l'employé à ajouter.\n"+"CIN de l'employé à ajouter:");
+                    Scanner cinEmpScanner = new Scanner(System.in);
+                    int cinEmp = cinEmpScanner.nextInt();
+                    System.out.println("Tapez le télèphone de l'employé à ajouter.\n"+"Télèphone de l'employé à ajouter:");
+                    Scanner telephoneEmpScanner = new Scanner(System.in);
+                    int telephoneEmp = telephoneEmpScanner.nextInt();
+                    Employe emp = new Empolye(nomEmp, prenomEmp , cinEmp , telephoneEmp);
+                    String resultat = institutiton.add(emp,nomInstitution);
+                    System.out.println(resultat);
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    System.exit(0);
+            }
+        }
+
+
     }
 }
